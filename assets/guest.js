3eed81c6eacf4fd96ededa987e2674d9766d5bbc
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2023-09-28 08:00:57 +0300

    add loop and function inside the loop

--- assets/guest.js
+++ assets/guest.js
@@ -1,1 +1,23 @@
 const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
+
+
+
+
+
+
+for(i = 0; i >= 1; i++){
+    const admitted = document.querySelector('.admitted');
+
+const refused = document.querySelector('.refused');
+
+    if(i === 'Phil' || i === 'Lola'){
+        refused.textContent = 'Refuse: ';
+    }
+else {
+    admitted.textContent = 'Admit: ';
+}
+}
+
+refused.textContent += ;
+ admitted.textContent += ;
+
